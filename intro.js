3d03cff5aa28bae702cecdde69af92441cabaa22
Alisha Dsouza <alishamarinadsouza@gmail.com>
2025-05-09 13:13:22 +0530

    Update intro.js

--- intro.js
+++ intro.js
@@ -14,13 +14,21 @@ box2.addEventListener('mouseleave', () => {
   box2.style.transform = 'scale(1)';
 });
 
-const box3 = document.getElementById('button-msg');
+const box3 = document.getElementById('link3');
 box3.addEventListener('mouseenter', () => {
   box3.style.transform = 'scale(1.1)';
 });
 box3.addEventListener('mouseleave', () => {
   box3.style.transform = 'scale(1)';
 });
-box3.addEventListener('click', () => {
+
+const box4 = document.getElementById('button-msg');
+box4.addEventListener('mouseenter', () => {
+  box4.style.transform = 'scale(1.1)';
+});
+box4.addEventListener('mouseleave', () => {
+  box4.style.transform = 'scale(1)';
+});
+box4.addEventListener('click', () => {
   alert('Hiii! Thanks for viewing my task 1 :)');
 });
